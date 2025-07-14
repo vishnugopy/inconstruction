8d81a4dfbb37ac06a151fe89b26098aeb2024dfd
vishnugopy <vishnugoby0010@gmail.com>
2025-07-14 16:17:09 +0200

    Enhance InConstruction component by adding optional className prop and refining CSS styles for overlay and content blur effect.

--- src/InConstruction.tsx
+++ src/InConstruction.tsx
@@ -6,15 +6,17 @@ import "./InConstruction.css";
 export interface InConstructionProps {
   children: ReactNode;
   message?: string; // Optional custom message for the "Under Construction" notice
+  className?: string;
 }
 
 // Functional component with TypeScript support
 const InConstruction: React.FC<InConstructionProps> = ({
   children,
   message,
+  className,
 }) => {
   return (
-    <div className="in-construction-zone">
+    <div className={`in-construction-zone ${className}`}>
       <div className="overlay">
         <p>🚧{message || "This section is under construction "}🚧</p>
       </div>
